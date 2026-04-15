c8ce72e180effc062c1e44cc2cb42e359a9c77bf
hgmenna <hgemnna@hotmail.com>
2026-04-15 10:45:28 -0300

    previsualizacion de imagen

--- app/Filament/Resources/TournamentRegistrations/Schemas/TournamentRegistrationForm.php
+++ app/Filament/Resources/TournamentRegistrations/Schemas/TournamentRegistrationForm.php
@@ -205,7 +205,6 @@ class TournamentRegistrationForm
                 ->disk('public_path') // <--- Indispensable para que sea accesible vía URL
                 ->visibility('public') // <--- Asegura permisos de lectura
                 ->directory('pagos')
-                ->previewable(false)
                 ->openable(true)
                 ->getUploadedFileNameForStorageUsing(function (Get $get, TemporaryUploadedFile $file, $livewire): string {
                     // 1. Obtener el ID del jugador seleccionado
